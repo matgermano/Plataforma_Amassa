085f3aaa74e6cc900068452a958bbf14b661d5a9
Camila Rangel <camilafariasrangel@gmail.com>
2022-01-08 02:02:40 -0300

    feat: global styles

---
+++ plataforma-amassa/src/GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Medula+One&display=swap');
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Medula One', cursive;
+  }
+`;
+ 
+export default GlobalStyle;
