ab2cba057e953ead1c8b9780332079937144eaca
Camila Rangel <camilafariasrangel@gmail.com>
2022-01-10 02:35:40 -0300

    feat: banner

--- plataforma-amassa/src/App.js
+++ plataforma-amassa/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar'
+import Banner from './components/Banner/Banner'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Navbar/> 
+    <Banner/>
     </div>   
   );
 }
---
+++ plataforma-amassa/src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Banner.css";
+
+class Banner extends Component {
+  render() {
+    return (
+        <img className="Banner" src="./banner.png" />
+    );
+  }
+}
+
+export default Banner;
