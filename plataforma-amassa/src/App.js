6f19f607ac6f2269fac6ff9a2fac6e901500b986
Matheus <germano.prog@gmail.com>
2022-01-10 22:37:38 -0300

    Refac: Identação App.js

--- plataforma-amassa/src/App.js
+++ plataforma-amassa/src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar'
-import Banner from './components/Banner/Banner'
-import Opcoes from './components/Opcoes/Opcoes'
-import Footer from './components/Footer/Footer'
-import './index.css';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Banner from "./components/Banner/Banner";
+import Opcoes from "./components/Opcoes/Opcoes";
+import Footer from "./components/Footer/Footer";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-    <Navbar/> 
-    <Banner/>
-    <Opcoes/>
-    <Footer/>
-    
-    </div>   
+    <>
+      <Navbar />
+      <Banner />
+      <Opcoes />
+      <Footer />
+    </>
   );
 }
 
