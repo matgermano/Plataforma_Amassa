1ca125fe61610bf4ece071919ed3fd09a423a8a8
Matheus <germano.prog@gmail.com>
2022-01-10 23:33:20 -0300

    Refac: Uso de Hooks | Modal2

--- plataforma-amassa/src/App.js
+++ plataforma-amassa/src/App.js
@@ -1,17 +1,22 @@
-import React from "react";
+import {React, useState} from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner";
 import Opcoes from "./components/Opcoes/Opcoes";
 import Footer from "./components/Footer/Footer";
 import "./index.css";
+import Modal from "./components/Modal/Modal";
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalVisible2, setIsModalVisible2] = useState(false);
   return (
     <>
-      <Navbar />
+      <Navbar setIsOpen={setIsModalVisible} setIsOpen2={setIsModalVisible2}/>
       <Banner />
       <Opcoes />
       <Footer />
+      {isModalVisible ? <Modal setIsModalVisible={setIsModalVisible} /> : null}
+      {isModalVisible2 ? <Modal setIsModalVisible={setIsModalVisible2} /> : null}
     </>
   );
 }
