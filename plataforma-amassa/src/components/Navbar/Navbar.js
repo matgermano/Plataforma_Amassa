ed31a573f2afe4e683ec70cea768aa54f751923a
Matheus <germano.prog@gmail.com>
2022-01-10 22:42:30 -0300

    Feat: Refatorado Navbar de Class para funcional incluindo modal

--- plataforma-amassa/src/components/Navbar/Navbar.js
+++ plataforma-amassa/src/components/Navbar/Navbar.js
@@ -1,33 +1,47 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { ItensMenu } from "./ItensMenu";
 import "./Navbar.css";
+import Modal from "../Modal/Modal";
 
-class Navbar extends Component {
-  state = { clicked: false };
-  handleClick = () => {
-    this.setState({clicked: !this.state.clicked});
-  }
+function Navbar() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
-  render() {
-    return (
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  console.log(isModalVisible);
+
+  return (
+    <>
       <nav className="ItensNavbar">
         <img className="LogoNavbar" src="./logo.png" />
-        <div className="menu-icon" onClick={this.handleClick}>
-          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
 
-        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-          {ItensMenu.map((item, i) => {
-            return (
-              <li key={i}>
-                <a className={item.cName}>{item.title}</a>
-              </li>
-            );
-          })}
+        <ul className={clicked ? "nav-menu active" : "nav-menu"}>
+          <li onClick={() => setIsModalVisible(true)}>
+            <a className="nav-links">
+              <h1>
+                <i class="fas fa-map-marker-alt"></i> Endereço de entrega
+              </h1>
+            </a>
+          </li>
+
+          <li onClick={() => setIsModalVisible(true)}>
+            <a className="nav-links">
+              <h1>
+                <i class="fas fa-shopping-bag"></i>Carrinho
+              </h1>
+            </a>
+          </li>
         </ul>
       </nav>
-    );
-  }
+      {isModalVisible ? <Modal /> : null}
+    </>
+  );
 }
 
 export default Navbar;
