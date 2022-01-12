aa4255d2ab964b8570abef442befd479bd119577
Matheus <germano.prog@gmail.com>
2022-01-12 00:18:14 -0300

    Refac: Atualização de modal

--- plataforma-amassa/src/components/Modal/ModalCarrinho.js
+++ plataforma-amassa/src/components/Modal/ModalCarrinho.js
@@ -1,25 +1,78 @@
 import React from "react";
 import "./ModalCarrinho.css";
 
-function ModalCarrinho({setIsModalVisible2}) {
+function ModalCarrinho({ setIsModalVisible2 }) {
   return (
     <>
-      <div onClick={()=> setIsModalVisible2(false)} id="dvmodal" className="modal">
+      <div id="dvmodal" className="modal">
         <div className="modal-content">
           <div className="modal-header">
-            <h1>AMEM SENHOR</h1>
+            <img className="LogoNavbar" src="./logo.png" />
+            <svg
+              onClick={() => setIsModalVisible2(false)}
+              className="svg"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#000000"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+            </svg>
           </div>
           <div className="modal-body">
-            <p>
-              Maecenas tempus faucibus euismod. Nunc turpis est, scelerisque sed
-              sodales sit amet, viverra ac ligula. Cras ut finibus ante. Morbi
-              eu convallis nisi, quis tempus ante. Quisque eu ornare lacus,
-              laoreet venenatis erat. Aenean diam justo, suscipit non sem eu,
-              consequat condimentum justo. Curabitur mollis risus ut malesuada
-              sodales. Donec eget fermentum magna. Nullam vel varius libero,
-              eget congue magna. Nam in turpis pulvinar, volutpat arcu in,
-              sollicitudin ante. Ut quis aliquam purus.
-            </p>
+            <div>Onde Você deseja receber seu pedido?</div>
+            <div>
+              <input
+                className="input"
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="CEP"
+              />
+            </div>
+            <div>
+              <input
+                className="input"
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="Bairro"
+              />
+            </div>
+            <div>
+              <input
+                className="input"
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="Rua"
+              />
+            </div>
+            <div>
+              <input
+                className="input"
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="Número"
+              />
+            </div>
+            <div>
+              <input
+                className="input"
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="Complemento"
+              />
+            </div>
+            <div>
+              <button className="btn" onClick={() => setIsModalVisible2(false)}>
+                Salvar
+              </button>
+            </div>
           </div>
           <div className="modal-footer"></div>
         </div>
--- plataforma-amassa/src/components/Modal/ModalEndereço.js
+++ plataforma-amassa/src/components/Modal/ModalEndereço.js
@@ -1,47 +1,80 @@
 import React from "react";
 import "./ModalEndereço.css";
-import "./delete.svg"
 
-function ModalEndereço({setIsModalVisible}) {
+function ModalEndereço({ setIsModalVisible }) {
   return (
     <>
-      <div  id="dvmodal" className="modal">
+      <div id="dvmodal" className="modal">
         <div className="modal-content">
           <div className="modal-header">
-          <img className="LogoNavbar" src="./logo.png" onClick={()=> setIsModalVisible(false)} />
-          <img src="./delete.svg"/>
+            <img className="LogoNavbar" src="./logo.png" />
+            <svg
+              onClick={() => setIsModalVisible(false)}
+              className="svg"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#000000"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+            </svg>
           </div>
           <div className="modal-body">
-            <div><p>
-              Onde Você deseja receber seu pedido?
-            </p>
-            </div>
-            <div><p>
-              <input className="input" type="text" name="nome" id="nome" placeholder="CEP"/>
-            </p>
-            </div>
-            <div><p>
-              <input className="input" type="text" name="nome" id="nome" placeholder="Bairro"/>
-            </p>
-            </div>
-            <div><p>
-              <input className="input" type="text" name="nome" id="nome" placeholder="Rua"/>
-            </p>
-            </div>
-            <div><p>
-              <input className="input" type="text" name="nome" id="nome" placeholder="Número"/>
-            </p>
-            </div>
-            <div><p>
-              <input className="input" type="text" name="nome" id="nome" placeholder="Complemento"/>
-            </p>
+            <div>Onde Você deseja receber seu pedido?</div>
+            <div>
+              <input
+                className="input"
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="CEP"
+              />
             </div>
             <div>
-            <button className="btn" onClick={()=> setIsModalVisible(false)}>Salvar</button>
+              <input
+                className="input"
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="Bairro"
+              />
+            </div>
+            <div>
+              <input
+                className="input"
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="Rua"
+              />
+            </div>
+            <div>
+              <input
+                className="input"
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="Número"
+              />
+            </div>
+            <div>
+              <input
+                className="input"
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="Complemento"
+              />
+            </div>
+            <div>
+              <button className="btn" onClick={() => setIsModalVisible(false)}>
+                Salvar
+              </button>
             </div>
           </div>
           <div className="modal-footer"></div>
-          
         </div>
       </div>
     </>
