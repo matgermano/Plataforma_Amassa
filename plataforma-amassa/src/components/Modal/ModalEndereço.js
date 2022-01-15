500fec168b002707674b8df171b3c99d143bb22a
Camila Rangel <camilafariasrangel@gmail.com>
2022-01-15 15:21:58 -0300

    feat: estilização modal de entrega

--- plataforma-amassa/src/components/Modal/ModalEndereço.js
+++ plataforma-amassa/src/components/Modal/ModalEndereço.js
@@ -6,72 +6,77 @@ function ModalEndereço({ setIsModalVisible }) {
     <>
       <div id="dvmodal" className="modalEnd">
         <div className="modal-content">
-          <div className="modal-header">
-            <img className="LogoNavbar" src="./logo.png" />
-            <svg
-              onClick={() => setIsModalVisible(false)}
-              className="svg"
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 0 24 24"
-              width="24px"
-              fill="#000000"
-            >
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
-            </svg>
-          </div>
+          <div className="modal-header"></div>
           <div className="modal-body">
-            <div>Onde Você deseja receber seu pedido?</div>
-            <div>
-              <input
-                className="input"
-                type="text"
-                name="nome"
-                id="nome"
-                placeholder="CEP"
-              />
-            </div>
-            <div>
-              <input
-                className="input"
-                type="text"
-                name="nome"
-                id="nome"
-                placeholder="Bairro"
-              />
-            </div>
-            <div>
-              <input
-                className="input"
-                type="text"
-                name="nome"
-                id="nome"
-                placeholder="Rua"
-              />
-            </div>
-            <div>
-              <input
-                className="input"
-                type="text"
-                name="nome"
-                id="nome"
-                placeholder="Número"
-              />
-            </div>
-            <div>
-              <input
-                className="input"
-                type="text"
-                name="nome"
-                id="nome"
-                placeholder="Complemento"
-              />
-            </div>
-            <div>
-              <button className="btn" onClick={() => setIsModalVisible(false)}>
-                Salvar
-              </button>
+            <div className="modal-form">
+              <div className="modal-titulo">
+                Onde deseja receber seu pedido?
+              </div>
+              <div className="form-flex">
+                <div className="input-cep">
+                  <input
+                    className="input"
+                    type="text"
+                    name="nome"
+                    id="nome"
+                    placeholder="CEP"
+                  />
+                </div>
+                <div className="input-bairro">
+                  <input
+                    className="input"
+                    type="text"
+                    name="nome"
+                    id="nome"
+                    placeholder="Bairro"
+                  />
+                </div>
+              </div>
+
+              <div className="input-rua">
+                <input
+                  className="input"
+                  type="text"
+                  name="nome"
+                  id="nome"
+                  placeholder="Rua"
+                />
+              </div>
+
+              <div className="form-flex">
+                <div className="input-numero">
+                  <input
+                    className="input"
+                    type="text"
+                    name="nome"
+                    id="nome"
+                    placeholder="Número"
+                  />
+                </div>
+                <div className="input-complemento">
+                  <input
+                    className="input"
+                    type="text"
+                    name="nome"
+                    id="nome"
+                    placeholder="Complemento"
+                  />
+                </div>
+              </div>
+              <div className="form-flex">
+                <button
+                  className="btn"
+                  onClick={() => setIsModalVisible(false)}
+                >
+                  Salvar
+                </button>
+                <button
+                  className="btn-2"
+                  onClick={() => setIsModalVisible(false)}
+                >
+                  Cancelar
+                </button>
+              </div>
             </div>
           </div>
           <div className="modal-footer"></div>
