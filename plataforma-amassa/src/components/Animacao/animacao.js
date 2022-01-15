240f1b4fe6f7c68316fd29c137f102377c492c8a
Matheus <germano.prog@gmail.com>
2022-01-15 13:20:13 -0300

    Style: Reformulando a animação

--- plataforma-amassa/src/components/Animacao/animacao.js
+++ plataforma-amassa/src/components/Animacao/animacao.js
@@ -5,12 +5,12 @@ function Animacao (){
     return(
         <div className="body-animacao">
         <div className="flex-animacao">
-        <div className="gradient-animacao"></div>
-            <div className="back-animacao"></div>
+        <div className="gradient-animacao">
+            <div className="back-animacao">
             <img className="img-animacao" src="./logo.png" alt=""/>
             <div className="shade-animacao"></div>
             <div className="logo-flex-animacao">
-        </div>
+        </div></div></div>
     </div>
     </div>
     );
