74d4677e03e2f1fab0b2de20ade7fbfd4fa102f6
Matheus <germano.prog@gmail.com>
2022-01-15 18:02:16 -0300

    Feat: integração Amassa_API

--- plataforma-amassa/src/App.js
+++ plataforma-amassa/src/App.js
@@ -1,5 +1,5 @@
-import {React, useState, useEffect} from "react";
-// import axios from "axios";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner";
 import Opcoes from "./components/Opcoes/Opcoes";
@@ -11,27 +11,49 @@ import ModalCarrinho from "./components/Modal/ModalCarrinho";
 import Animacao from "./components/Animacao/animacao.js";
 
 function App() {
-  // useEffect(()=>{
-  //   axios.get('https://amassapi.herokuapp.com/pratos-principais')
-  //   .then((response)=>{
-  //     const chama= []
-  //     console.log(response);
-  //   }) .catch((error)=>{
-  //     console.log(error);
-  //   })
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/pratos-principais")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
-  // });
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/bebidas")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/entradas")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isModalVisible2, setIsModalVisible2] = useState(false);
   return (
     <>
       <Navbar setIsOpen={setIsModalVisible} setIsOpen2={setIsModalVisible2} />
-
+      <Animacao />
       <Banner />
 
       <Opcoes />
-      <Cards/>
+      <Cards />
       <Footer />
       {isModalVisible ? (
         <ModalEndereço setIsModalVisible={setIsModalVisible} />
